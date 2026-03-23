4d16f7d3ef691450781ae5d7940c8d176549a292
Qin Wang <qincorfu@gmail.com>
2026-03-23 10:00:29 +0800

    fix: allow DATABASE_URL override for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,7 @@
 from logging.config import fileConfig
 
+import os
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -9,6 +11,10 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -16,6 +22,7 @@ if config.config_file_name is not None:
 
 # Import models for autogenerate support
 from risklens.db.models import Base
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -62,9 +69,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
